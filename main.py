917695bd33e12707b6f379e469b64fa0f1b7b41a
abduljawad991 <70905521+abduljawad991@users.noreply.github.com>
2021-10-28 04:15:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ a=int(input("type a number: "))
 b=int(input("type another number: "))
 s=a+b
 print("sum=",s)
+
+x=int(input("type a number: "))
+y=int(input("type another number: "))
+sub=x-y
+print("x-y=",sub)
